759dad558cf3ca97cddd9c1bb3be4f216289685f
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-12-15 18:35:29 -0300

    chore: created profile component structure

--- components/nav/Navbar.tsx
+++ components/nav/Navbar.tsx
@@ -3,13 +3,12 @@
 import Link from 'next/link'
 
 import { LoginForm } from './LoginForm'
+import { Profile } from './Profile'
 
 import { useUserStore } from '@/lib/store/user'
 
 export function Navbar() {
   const user = useUserStore((state) => state.user)
-
-  console.log(user)
 
   return (
     <nav className="flex items-center justify-between">
@@ -19,7 +18,7 @@ export function Navbar() {
         </Link>
         <div className="h-1 w-0 group-hover:w-full transition-all bg-green-500"></div>
       </div>
-      {user?.id ? <h1>Profile</h1> : <LoginForm />}
+      {user?.id ? <Profile /> : <LoginForm />}
     </nav>
   )
 }
---
+++ components/nav/Profile.tsx
@@ -0,0 +1,54 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import { DashboardIcon, LockOpen1Icon } from '@radix-ui/react-icons'
+
+import { useUserStore } from '@/lib/store/user'
+
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from '@/components/ui/popover'
+import { Button } from '@/components/ui/button'
+
+export function Profile() {
+  const user = useUserStore((state) => state.user)
+
+  return (
+    <Popover>
+      <PopoverTrigger>
+        <Image
+          src={user?.user_metadata.avatar_url}
+          alt={user?.user_metadata.user_name}
+          width={50}
+          height={50}
+          className="rounded-full ring-2 ring-green-500"
+        />
+      </PopoverTrigger>
+      <PopoverContent className="p-2 space-y-3 divide-y">
+        <div className="px-4 text-sm">
+          <p>{user?.user_metadata.user_name}</p>
+          <p className="text-gray-500">{user?.user_metadata.email}</p>
+        </div>
+        <Link href="/dashboard" className="block">
+          <Button
+            variant="ghost"
+            className="w-full flex items-center justify-between"
+          >
+            Dashboard
+            <DashboardIcon />
+          </Button>
+        </Link>
+        <Link href="/dashboard" className="block">
+          <Button
+            variant="ghost"
+            className="w-full flex items-center justify-between"
+          >
+            Logout
+            <LockOpen1Icon />
+          </Button>
+        </Link>
+      </PopoverContent>
+    </Popover>
+  )
+}
