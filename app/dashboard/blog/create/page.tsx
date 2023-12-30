37521db7eac1be431e503e0a201c58d981a43773
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2023-12-30 03:57:12 -0300

    chore: added customized message if user did not provided supported cdn hostname

--- app/dashboard/blog/create/page.tsx
+++ app/dashboard/blog/create/page.tsx
@@ -29,17 +29,32 @@ import { Textarea } from '@/components/ui/textarea'
 import { toast } from '@/components/ui/use-toast'
 import { MarkdownPreview } from '@/components/markdown/MarkdownPreview'
 
-const FormSchema = z.object({
-  title: z.string().min(2, {
-    message: 'Title must be at least 2 characters.',
-  }),
-  image_url: z.string().url({ message: 'Invalid URL' }),
-  content: z.string().min(2, {
-    message: 'Content must be at least 2 characters.',
-  }),
-  is_published: z.boolean(),
-  is_premium: z.boolean(),
-})
+const FormSchema = z
+  .object({
+    title: z.string().min(2, {
+      message: 'Title must be at least 2 characters.',
+    }),
+    image_url: z.string().url({ message: 'Invalid URL' }),
+    content: z.string().min(2, {
+      message: 'Content must be at least 2 characters.',
+    }),
+    is_published: z.boolean(),
+    is_premium: z.boolean(),
+  })
+  .refine(
+    (data) => {
+      const image_url = data.image_url
+
+      try {
+        const url = new URL(image_url)
+
+        return url.hostname === 'image.unsplash.com'
+      } catch {
+        return false
+      }
+    },
+    { message: 'Currently unsupported cdn of images', path: ['image_url'] }
+  )
 
 export default function BlogForm() {
   const [isPreview, setIsPreview] = useState(false)
